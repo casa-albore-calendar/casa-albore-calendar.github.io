98488dec97d3bf014119191607d267dedc2f42f2
Nick Stephen <nick.stephen@oracle.com>
2017-01-29 23:28:24 +0100

    update

--- copy-calendars.sh
+++ copy-calendars.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd `dirname $0`
+
 curl https://calendar.google.com/calendar/ical/h0cl2pufaic02ubqj1cdlr9ur0%40group.calendar.google.com/public/basic.ics > bas.ics
 curl https://calendar.google.com/calendar/ical/64f2d319jcgv1grt6ae2h3erqg%40group.calendar.google.com/public/basic.ics > haut.ics
 
